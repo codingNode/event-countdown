c519438d841b72625695095e641f61e607b7e319
Hasnat Gulbaz <hasnat.uetian145@gmail.com>
2022-12-07 22:55:51 +0500

    final

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const dateEl= document.getElementById('date-picker');
 const countForm = document.getElementById('countdownForm')
-
-
+const resetBtn = document.getElementById('countdown-button')
+const inputEl= document.getElementById('input-container')
+const countDown = document.getElementById('countdown')
+const countTitleEl = document.getElementById('countdown-title')
+const completeEl= document.getElementById('complete')
+const completeBtn = document.getElementById('complete-button')
 
 // setting the min attribute of the date-picket to today
 const today = new Date().toISOString().split('T')[0]
@@ -10,8 +14,9 @@ dateEl.setAttribute('min',today)
 
 
 let countTitle = '';
-let countDate = ''
-
+let countDate = '';
+let countActive;
+let savedCount;
 let countdownValue = Date;
 
 
@@ -25,34 +30,42 @@ function updateDom()
     const timeNow = new Date()
     const now = timeNow.getTime()
 
-    console.log('difference in time zone w.r.t UTC', (timeNow.getTimezoneOffset())/60)
+    // console.log('difference in time zone w.r.t UTC', (timeNow.getTimezoneOffset())/60)
     const countValue = countdownValue-now;
-    console.log('countdown', countValue)
 
+    console.log('countdown', countValue)
+    if(countValue<0)
+    {
+        const completeInfo = document.getElementById('complete-info');
+
+        completeEl.hidden = false;
+        inputEl.hidden = true;
+        countDown.hidden = true;
+
+        completeInfo.textContent =`${countTitle} has ended on ${countDate}`;
+        clearInterval(countActive);
+    }
+    else
+    {
+        
     const days = Math.floor(countValue/day);
     const hours = Math.floor(((countValue % day)/hour)+(timeNow.getTimezoneOffset()/60));
     const mins = Math.floor((countValue % hour)/min);
     const secs = Math.floor((countValue % min)/sec);
-    console.log('days',days,"Hours" ,hours, 'Minutes:', mins, 'Seconds:', secs)
-    
+    // console.log('days',days,"Hours" ,hours, 'Minutes:', mins, 'Seconds:', secs)
+
     const countList = document.querySelectorAll('span')
     countList[0].innerText = `${days}`;
     countList[1].innerText = `${hours}`;
     countList[2].innerText = `${mins}`;
     countList[3].innerText = `${secs}`;
 
-
-    const inputEl= document.getElementById('input-container')
     inputEl.hidden = true;
 
-    const countDown = document.getElementById('countdown')
     countDown.hidden = false;
 
-    const countTitleEl = document.getElementById('countdown-title')
-    countTitleEl.textContent = `${countTitle}`;
-
-    
-
+    countTitleEl.textContent = `${countTitle}`;       
+    }
     
 }
 
@@ -64,12 +77,64 @@ function countUpdate(e)
     countTitle = e.srcElement[0].value;
     countDate = e.srcElement[1].value;
 
-    countdownValue = new Date(countDate).getTime()
+    if(countDate === '')
+    {
+        alert('Please select the date!')
+    }
+    else
+    {
+        countdownValue = new Date(countDate).getTime()
 
-    console.log('since jan 1970',countdownValue)
+        savedCount={
+            'title' : countTitle,
+            'date' : countDate
+        }
 
-    updateDom()
+        localStorage.setItem('countdown', JSON.stringify(savedCount))
+        // console.log('since jan 1970',countdownValue)
+        countActive = setInterval(updateDom,1000)
+        // updateDom()
+    }
+
 
 }
 
-countForm.addEventListener('submit',countUpdate)
+function restoreLocalStorage()
+{
+    if(localStorage.getItem('countdown'))
+    {
+        inputEl.hidden = true;
+        
+        savedCount = JSON.parse(localStorage.getItem('countdown'));
+        console.log(savedCount)
+        countTitle = savedCount.title;
+        countDate = savedCount.date;
+        countdownValue = new Date(countDate).getTime();
+        countActive = setInterval(updateDom,1000)
+    }
+    
+}
+
+function reset(e)
+{
+    
+    countTitle='';
+    countDate='';
+
+    const inputEl= document.getElementById('input-container')
+    inputEl.hidden = false;
+
+    const countDown = document.getElementById('countdown')
+    countDown.hidden = true;
+    completeEl.hidden = true;
+
+    // clearTimeout(countActive)
+    clearInterval(countActive)
+    localStorage.removeItem('countdown')
+}
+
+countForm.addEventListener('submit',countUpdate);
+resetBtn.addEventListener('click', reset);
+completeBtn.addEventListener('click', reset);
+
+restoreLocalStorage();
